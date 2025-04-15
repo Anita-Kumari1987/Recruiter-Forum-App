305142723f99bb6fdca512aea21f23cbd68f06ab
Anita-Kumari1987 <anita.kumari1987@yahoo.com>
2025-04-15 19:13:10 +0200

    Changed folder striucture

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./Style/index.css";
 import App from "./App.jsx";
 
 import "sweetalert2/src/sweetalert2.scss";
